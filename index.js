c10ae993dbbefad0f4e3f1fbe3d13e835cad2bc8
julioMarvim <juliomarvim@yahoo.com.br>
2023-05-06 20:44:50 -0300

    feat: trabalhando com meddlewares

--- index.js
+++ index.js
@@ -5,6 +5,28 @@ server.use(express.json())
 
 const cursos = ['Node.js', 'JavaScript', 'React Native']
 
+//Criando um middleware global
+server.use((req, res, next) =>{
+  console.log(`Url chamada ${req.url}`);
+  return next();
+})
+
+//Criando um middleware especifico.
+function checkCurso(req, res, next){
+  if(!req.body.name){
+    return res.status(400).json({error: 'Nome do curso é obrigatório.'});
+  }
+
+  return next();
+}
+
+function checkIndexCurso(req, res, next) {
+  const { index } = req.params;
+  if(!cursos.indexOf(index)){
+    return res.status(404).json({error: 'Id informado não encontrado.'})
+  }
+}
+
 server.get('/cursos/:index', (req, res) => {
   const { index } = req.params;
   return res.json(cursos[index]);
@@ -14,13 +36,13 @@ server.get('/cursos', (req, res) =>{
   return res.json(cursos); 
 })
 
-server.post('/cursos', (req, res) =>{
+server.post('/cursos', checkCurso, (req, res) =>{
   const { name } = req.body;
   cursos.push(name);
   return res.json(cursos);
 })
 
-server.put('/cursos/:index', (req, res) =>{
+server.put('/cursos/:index', checkCurso, (req, res) =>{
   const { index } = req.params;
   const { name }  = req.body;
 
